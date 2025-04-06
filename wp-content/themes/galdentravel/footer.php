85f6d8d81f3b356c2d2bbd43f1f1160b28c1ea07
DarkGalen <carlosgaleanotorres@gmail.com>
2025-04-06 22:19:48 +0200

    version final 1.2

--- wp-content/themes/galdentravel/footer.php
+++ wp-content/themes/galdentravel/footer.php
@@ -1,8 +1,18 @@
 <footer class="site-footer">
+  <div class="container-logo">
+    <div class="logo">
+      <a href="<?php echo esc_url(home_url('/')); ?>">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/logo_sin_fondo.png" alt="logo IMG">
+      </a>
+      <h3> Galden Travel </h3>
+    </div>
+  </div>
+
     <p class="p-container">
-    <?php echo "All rights reserved <span>" . get_bloginfo('name') . "</span> " . date('Y'); ?>
-  </p>
-  </footer>
-  <?php wp_footer(); ?>
+      <?php echo "© 1999 - 2025 Galden Travel. All rights reserved. Galden Travel, S.L. (Sole Proprietorship). Registered Office (not open to the public): Calle de Manzanares, No. 4, 1st Floor, Office 108, 28005, Madrid, Spain. VAT Number: ESB-61965778. Registered in the Madrid Commercial Registry, Volume 36897, Page 121, Sheet M-660117. Travel Agency License: CICMA 3682 and IATA Accredited Agent. Customer Support: Please use our Help Center or contact our agents here." ?>
+    </p>
+</footer>
+<?php wp_footer(); ?>
 </body>
+
 </html>
